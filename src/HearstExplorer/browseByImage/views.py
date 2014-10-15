2edb0e9a55b2098e87267843e870b88df050e62e
Robert Hui <legatek@gmail.com>
2014-10-15 11:32:46 -0700

    Removed api key from source.

--- src/HearstExplorer/browseByImage/views.py
+++ src/HearstExplorer/browseByImage/views.py
@@ -6,8 +6,8 @@ import json
 # Create your views here
 def browse(request):
     client = CollectionSpaceClient(
-        app_id='50090494',
-        app_key='2080f7ee22be1e55783f8f9d8b631f82'
+        app_id='/*appId goes here*/',
+        app_key='/*appKey goes here*/'
     )
     json_data = client.fetch()
     artifacts = client.parse(json_data)
@@ -20,8 +20,8 @@ def browse(request):
 
 def detail(request, artifact_id):
     client = CollectionSpaceClient(
-        app_id='50090494',
-        app_key='2080f7ee22be1e55783f8f9d8b631f82'
+        app_id='/*appId goes here*/',
+        app_key='/*appKey goes here*/'
     )
     json_data = client.fetch_artifact(artifact_id)
     artifacts = client.parse(json_data)
@@ -36,8 +36,8 @@ def detail(request, artifact_id):
 
 def refinedBrowse(request, keyword):
     client = CollectionSpaceClient(
-        app_id='50090494',
-        app_key='2080f7ee22be1e55783f8f9d8b631f82'
+        app_id='/*appId goes here*/',
+        app_key='/*appKey goes here*/'
     )
     json_data = client.fetch(keyword=keyword)
     artifacts = client.parse(json_data)
